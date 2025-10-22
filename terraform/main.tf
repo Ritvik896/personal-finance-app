815ef7d43b06512b4ab7ec1c8ac39a9139d7e633
Ritvik <singhritvik275@gmail.com>
2025-10-23 01:35:55 +0530

    Fix: Use dynamic AMI lookup for Amazon Linux 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,8 +58,23 @@ resource "aws_db_instance" "postgres" {
 # -------------------------------
 # EC2 Instance for Flask + Docker
 # -------------------------------
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "flask" {
-  ami                    = "ami-0b0ea68c435eb488d" # Amazon Linux 2 (Mumbai)
+  ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = "t2.micro"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
